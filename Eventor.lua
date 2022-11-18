ea91fa64aa54d7351b114fb0243018f105f5ae56
Ek1 <seppa.erkki@gmail.com>
2022-11-18 20:20:32 +0200

    test

--- Eventor.lua
+++ Eventor.lua
@@ -2,7 +2,7 @@ Eventor = {
 	TITLE = "Eventor - Events Spam Online",	-- Not codereview friendly but enduser friendly version of the add-on's name
 	AUTHOR = "Ek1",
 	DESCRIPTION = "One stop event add-on about the numerous ticket giving ESO events to keep track what you have done, how many and when. Keeps up your exp buff too. Also warns if you can't fit any more tickets.",
-	VERSION = "1034.220630",
+	VERSION = "1036.221117",
 	VARIABLEVERSION = "32",
 	LIECENSE = "CC BY-SA 4.0 = Creative Commons Attribution-ShareAlike 4.0 International License",
 	URL = "https://github.com/Ek1/Eventor",
@@ -21,12 +21,13 @@ local eventorSettings = {	-- default settings
 	keepEventBuffsOn = true,
 	eventBuffsThreshold = 60,
 	lastTimeSomeoneGainedEventBuff = 1640820402,
-	whenOurEventBuffRunsOut = 1618495200,
 	lastTimeEventLootWasGained = 1640820402,
+	whenOurEventBuffRunsOut = 1618495200,
 }
 
 local alarmsRemaining = eventorSettings.alarmAnnoyance or 9999
-local dailysReseted = os.time() + GetTimedActivityTimeRemainingSeconds(4) - 86400	-- 24h*60min*60sec = 86400 seconds
+local dailysReseted = os.time() + TIMED_ACTIVITIES_MANAGER:GetTimedActivityTypeTimeRemainingSeconds(0) - 86400	-- 24h*60min*60sec = 86400 seconds
+-- if timedActivityId 
 --/script d (  ZO_FormatTimeLargestTwo( GetTimedActivityTimeRemainingSeconds(4)  , TIME_FORMAT_STYLE_DESCRIPTIVE_MINIMAL))
 local todaysYear = tonumber(os.date("%Y", dailysReseted))
 local todaysDate = tonumber(os.date("%Y%m%d", dailysReseted))
@@ -38,8 +39,178 @@ local EVENTLOOT = {
 	[156717] = 1,	-- Hefty Undaunted Reward Box
 	[171267] = 2,	-- Undaunted Reward Box
 	[171268] = 1, -- Glorious Undaunted Reward Box
-	[182317] = 2,	-- Undaunted Reward Box	2021-11-18
-	[182318] = 1, -- Glorious Undaunted Reward Box	2021-11-18
+	[182317] = 2, -- Undaunted Reward Box	2021-11-18
+	[182318] = 1, --	Glorious Undaunted Reward Box	2021-11-18
+	--	2022-09-08
+	[190173] = 1, --	Glorious Undaunted Box: Arx Corinium
+	[190166] = 1, --	Glorious Undaunted Box: Banished Cells
+	[190200] = 1, --	Glorious Undaunted Box: Black Drake Villa
+	[190178] = 1, --	Glorious Undaunted Box: Blackheart Haven
+	[190179] = 1, --	Glorious Undaunted Box: Blessed Crucible
+	[190187] = 1, --	Glorious Undaunted Box: Bloodroot Forge
+	[190198] = 1, --	Glorious Undaunted Box: Castle Thorn
+	[190174] = 1, --	Glorious Undaunted Box: City of Ash
+	[190204] = 1, --	Glorious Undaunted Box: Coral Aerie
+	[190185] = 1, --	Glorious Undaunted Box: Cradle of Shadows
+	[190172] = 1, --	Glorious Undaunted Box: Crypt of Hearts
+	[190169] = 1, --	Glorious Undaunted Box: Darkshade Cavern
+	[190192] = 1, --	Glorious Undaunted Box: Depths of Malatar
+	[190175] = 1, --	Glorious Undaunted Box: Direfrost Keep
+	[190206] = 1, --	Glorious Undaunted Box: Earthen Root Enclave
+	[190170] = 1, --	Glorious Undaunted Box: Elden Hollow
+	[190186] = 1, --	Glorious Undaunted Box: Falkreath Hold
+	[190188] = 1, --	Glorious Undaunted Box: Fang Lair
+	[190193] = 1, --	Glorious Undaunted Box: Frostvault
+	[190167] = 1, --	Glorious Undaunted Box: Fungal Grotto
+	[190207] = 1, --	Glorious Undaunted Box: Graven Deep
+	[190196] = 1, --	Glorious Undaunted Box: Icereach
+	[190182] = 1, --	Glorious Undaunted Box: Imperial City Prison
+	[190195] = 1, --	Glorious Undaunted Box: Lair of Maarselok
+	[190191] = 1, --	Glorious Undaunted Box: March of Sacrifices
+	[190190] = 1, --	Glorious Undaunted Box: Moon Hunter
+	[190194] = 1, --	Glorious Undaunted Box: Moongrave Fane
+	[190202] = 1, --	Glorious Undaunted Box: Red Petal Bastion
+	[190184] = 1, --	Glorious Undaunted Box: Ruins of Mazzatun
+	[190189] = 1, --	Glorious Undaunted Box: Scalecaller Peak
+	[190180] = 1, --	Glorious Undaunted Box: Selene's Web
+	[190205] = 1, --	Glorious Undaunted Box: Shipwright's Regret
+	[190168] = 1, --	Glorious Undaunted Box: Spindleclutch
+	[190199] = 1, --	Glorious Undaunted Box: Stone Garden
+	[190176] = 1, --	Glorious Undaunted Box: Tempest Island
+	[190201] = 1, --	Glorious Undaunted Box: The Cauldron
+	[190203] = 1, --	Glorious Undaunted Box: The Dread Cellar
+	[190197] = 1, --	Glorious Undaunted Box: Unhallowed Grave
+	[190181] = 1, --	Glorious Undaunted Box: Vaults of Madness
+	[190131] = 1, --	Glorious Undaunted Box: Veteran Arx Corinium
+	[190124] = 1, --	Glorious Undaunted Box: Veteran Banished Cells
+	[190158] = 1, --	Glorious Undaunted Box: Veteran Black Drake Villa
+	[190136] = 1, --	Glorious Undaunted Box: Veteran Blackheart Haven
+	[190137] = 1, --	Glorious Undaunted Box: Veteran Blessed Crucible
+	[190145] = 1, --	Glorious Undaunted Box: Veteran Bloodroot Forge
+	[190156] = 1, --	Glorious Undaunted Box: Veteran Castle Thorn
+	[190132] = 1, --	Glorious Undaunted Box: Veteran City of Ash
+	[190162] = 1, --	Glorious Undaunted Box: Veteran Coral Aerie
+	[190143] = 1, --	Glorious Undaunted Box: Veteran Cradle of Shadows
+	[190130] = 1, --	Glorious Undaunted Box: Veteran Crypt of Hearts
+	[190127] = 1, --	Glorious Undaunted Box: Veteran Darkshade Cavern
+	[190150] = 1, --	Glorious Undaunted Box: Veteran Depths of Malatar
+	[190133] = 1, --	Glorious Undaunted Box: Veteran Direfrost Keep
+	[190164] = 1, --	Glorious Undaunted Box: Veteran Earthen Root
+	[190128] = 1, --	Glorious Undaunted Box: Veteran Elden Hollow
+	[190144] = 1, --	Glorious Undaunted Box: Veteran Falkreath Hold
+	[190146] = 1, --	Glorious Undaunted Box: Veteran Fang Lair
+	[190151] = 1, --	Glorious Undaunted Box: Veteran Frostvault
+	[190125] = 1, --	Glorious Undaunted Box: Veteran Fungal Grotto
+	[190165] = 1, --	Glorious Undaunted Box: Veteran Graven Deep
+	[190154] = 1, --	Glorious Undaunted Box: Veteran Icereach
+	[190140] = 1, --	Glorious Undaunted Box: Veteran Imperial City Pris
+	[190153] = 1, --	Glorious Undaunted Box: Veteran Lair of Maarselok
+	[190149] = 1, --	Glorious Undaunted Box: Veteran March of Sacrifice
+	[190148] = 1, --	Glorious Undaunted Box: Veteran Moon Hunter
+	[190152] = 1, --	Glorious Undaunted Box: Veteran Moongrave Fane
+	[190160] = 1, --	Glorious Undaunted Box: Veteran Red Petal Bastion
+	[190142] = 1, --	Glorious Undaunted Box: Veteran Ruins of Mazzatun
+	[190147] = 1, --	Glorious Undaunted Box: Veteran Scalecaller Peak
+	[190138] = 1, --	Glorious Undaunted Box: Veteran Selene's Web
+	[190163] = 1, --	Glorious Undaunted Box: Veteran Shipwright's Regre
+	[190126] = 1, --	Glorious Undaunted Box: Veteran Spindleclutch
+	[190157] = 1, --	Glorious Undaunted Box: Veteran Stone Garden
+	[190134] = 1, --	Glorious Undaunted Box: Veteran Tempest Island
+	[190159] = 1, --	Glorious Undaunted Box: Veteran the Cauldron
+	[190161] = 1, --	Glorious Undaunted Box: Veteran the Dread Cellar
+	[190155] = 1, --	Glorious Undaunted Box: Veteran Unhallowed Grave
+	[190139] = 1, --	Glorious Undaunted Box: Veteran Vaults of Madness
+	[190135] = 1, --	Glorious Undaunted Box: Veteran Volenfell
+	[190129] = 1, --	Glorious Undaunted Box: Veteran Wayrest Sewers
+	[190141] = 1, --	Glorious Undaunted Box: Veteran White-Gold Tower
+	[190177] = 1, --	Glorious Undaunted Box: Volenfell
+	[190171] = 1, --	Glorious Undaunted Box: Wayrest Sewers
+	[190183] = 1, --	Glorious Undaunted Box: White-Gold Tower
+	[190257] = 2, --	Undaunted Box: Arx Corinium
+	[190250] = 2, --	Undaunted Box: Banished Cells
+	[190284] = 2, --	Undaunted Box: Black Drake Villa
+	[190262] = 2, --	Undaunted Box: Blackheart Haven
+	[190263] = 2, --	Undaunted Box: Blessed Crucible
+	[190271] = 2, --	Undaunted Box: Bloodroot Forge
+	[190282] = 2, --	Undaunted Box: Castle Thorn
+	[190258] = 2, --	Undaunted Box: City of Ash
+	[190288] = 2, --	Undaunted Box: Coral Aerie
+	[190269] = 2, --	Undaunted Box: Cradle of Shadows
+	[190256] = 2, --	Undaunted Box: Crypt of Hearts
+	[190253] = 2, --	Undaunted Box: Darkshade Cavern
+	[190276] = 2, --	Undaunted Box: Depths of Malatar
+	[190259] = 2, --	Undaunted Box: Direfrost Keep
+	[190290] = 2, --	Undaunted Box: Earthen Root Enclave
+	[190254] = 2, --	Undaunted Box: Elden Hollow
+	[190270] = 2, --	Undaunted Box: Falkreath Hold
+	[190272] = 2, --	Undaunted Box: Fang Lair
+	[190277] = 2, --	Undaunted Box: Frostvault
+	[190251] = 2, --	Undaunted Box: Fungal Grotto
+	[190291] = 2, --	Undaunted Box: Graven Deep
+	[190280] = 2, --	Undaunted Box: Icereach
+	[190266] = 2, --	Undaunted Box: Imperial City Prison
+	[190279] = 2, --	Undaunted Box: Lair of Maarselok
+	[190275] = 2, --	Undaunted Box: March of Sacrifices
+	[190274] = 2, --	Undaunted Box: Moon Hunter
+	[190278] = 2, --	Undaunted Box: Moongrave Fane
+	[190286] = 2, --	Undaunted Box: Red Petal Bastion
+	[190268] = 2, --	Undaunted Box: Ruins of Mazzatun
+	[190273] = 2, --	Undaunted Box: Scalecaller Peak
+	[190264] = 2, --	Undaunted Box: Selene's Web
+	[190289] = 2, --	Undaunted Box: Shipwright's Regret
+	[190252] = 2, --	Undaunted Box: Spindleclutch
+	[190283] = 2, --	Undaunted Box: Stone Garden
+	[190260] = 2, --	Undaunted Box: Tempest Island
+	[190285] = 2, --	Undaunted Box: The Cauldron
+	[190287] = 2, --	Undaunted Box: The Dread Cellar
+	[190281] = 2, --	Undaunted Box: Unhallowed Grave
+	[190265] = 2, --	Undaunted Box: Vaults of Madness
+	[190215] = 2, --	Undaunted Box: Veteran Arx Corinium
+	[190208] = 2, --	Undaunted Box: Veteran Banished Cells
+	[190242] = 2, --	Undaunted Box: Veteran Black Drake Villa
+	[190220] = 2, --	Undaunted Box: Veteran Blackheart Haven
+	[190221] = 2, --	Undaunted Box: Veteran Blessed Crucible
+	[190229] = 2, --	Undaunted Box: Veteran Bloodroot Forge
+	[190240] = 2, --	Undaunted Box: Veteran Castle Thorn
+	[190216] = 2, --	Undaunted Box: Veteran City of Ash
+	[190246] = 2, --	Undaunted Box: Veteran Coral Aerie
+	[190227] = 2, --	Undaunted Box: Veteran Cradle of Shadows
+	[190214] = 2, --	Undaunted Box: Veteran Crypt of Hearts
+	[190211] = 2, --	Undaunted Box: Veteran Darkshade Cavern
+	[190234] = 2, --	Undaunted Box: Veteran Depths of Malatar
+	[190217] = 2, --	Undaunted Box: Veteran Direfrost Keep
+	[190248] = 2, --	Undaunted Box: Veteran Earthen Root Enclave
+	[190212] = 2, --	Undaunted Box: Veteran Elden Hollow
+	[190228] = 2, --	Undaunted Box: Veteran Falkreath Hold
+	[190230] = 2, --	Undaunted Box: Veteran Fang Lair
+	[190235] = 2, --	Undaunted Box: Veteran Frostvault
+	[190209] = 2, --	Undaunted Box: Veteran Fungal Grotto
+	[190249] = 2, --	Undaunted Box: Veteran Graven Deep
+	[190238] = 2, --	Undaunted Box: Veteran Icereach
+	[190224] = 2, --	Undaunted Box: Veteran Imperial City Prison
+	[190237] = 2, --	Undaunted Box: Veteran Lair of Maarselok
+	[190233] = 2, --	Undaunted Box: Veteran March of Sacrifices
+	[190232] = 2, --	Undaunted Box: Veteran Moon Hunter
+	[190236] = 2, --	Undaunted Box: Veteran Moongrave Fane
+	[190244] = 2, --	Undaunted Box: Veteran Red Petal Bastion
+	[190226] = 2, --	Undaunted Box: Veteran Ruins of Mazzatun
+	[190231] = 2, --	Undaunted Box: Veteran Scalecaller Peak
+	[190222] = 2, --	Undaunted Box: Veteran Selene's Web
+	[190247] = 2, --	Undaunted Box: Veteran Shipwright's Regret
+	[190210] = 2, --	Undaunted Box: Veteran Spindleclutch
+	[190241] = 2, --	Undaunted Box: Veteran Stone Garden
+	[190218] = 2, --	Undaunted Box: Veteran Tempest Island
+	[190243] = 2, --	Undaunted Box: Veteran the Cauldron
+	[190245] = 2, --	Undaunted Box: Veteran the Dread Cellar
+	[190239] = 2, --	Undaunted Box: Veteran Unhallowed Grave
+	[190223] = 2, --	Undaunted Box: Veteran Vaults of Madness
+	[190219] = 2, --	Undaunted Box: Veteran Volenfell
+	[190213] = 2, --	Undaunted Box: Veteran Wayrest Sewers
+	[190225] = 2, --	Undaunted Box: Veteran White-Gold Tower
+	[190261] = 2, --	Undaunted Box: Volenfell
+	[190255] = 2, --	Undaunted Box: Wayrest Sewers
+	[190267] = 2, --	Undaunted Box: White-Gold Tower
+
 
 	-- W03	
 	[182599] = 2,	-- Daedric War Spoils
@@ -97,6 +268,10 @@ local EVENTLOOT = {
 	[181433] = 1,	-- 20	Glorious Blackwood Legates' Coffer
 	[178723] = 2,	-- 20	Blackwood Legates' Coffer
 
+	-- W39	Heroes of High Isle
+	[190059] = 1,	-- 22	Society of the Steadfast's Largesse
+	[190058] = 2,	-- 22	Society of the Steadfast's Munificence
+
 	-- W42	Witches Festival
 	[84521] = 2,	-- 16	Plunder Skull
 	[128358] = 2,	-- 17	Plunder Skull
@@ -132,6 +307,21 @@ local EVENTLOOT = {
 	[178691] = 1,	-- Dremora Plunder Skull, Public & Sweeper
 	[178692] = 1,	-- Dremora Plunder Skull, Trial
 	[178693] = 1,	-- Dremora Plunder Skull, World
+	[190013] = 1, --	22 Dremora Plunder Skull, Arena
+	[190014] = 1, --	22 Dremora Plunder Skull, Incursions
+	[190015] = 1, --	22 Dremora Plunder Skull, Delve
+	[190016] = 1, --	22 Dremora Plunder Skull, Dungeon
+	[190017] = 1, --	22 Dremora Plunder Skull, Public & Sweeper
+	[190018] = 1, --	22 Dremora Plunder Skull, Trial
+	[190019] = 1, --	22 Dremora Plunder Skull, World
+	[190038] = 1, --	22 Dremora Plunder Skull, Crowborne Horror
+	[190037] = 2, --	22 Plunder Skull
+
+	-- W46	Dark Heart of Skyrim Celebration
+	[167226] = 2, --	21 Box of Gray Host Pillage
+	[167227] = 1, --	21 Bulging Box of Gray Host Pillage
+	[193761] = 2, --	22 Box of Gray Host Pillage
+	[193762] = 1, --	22 Glorious Box of Gray Host Pillage
 
 	-- w50	New Life Festival
 	[96390] = 2,	-- 16	New Life Festival Box
@@ -145,6 +335,9 @@ local EVENTLOOT = {
 }
 
 local EVENTQUESTIDS = {
+	[6695] = 1, -- Witches Festival: Plucking the Crow
+
+	--	The New Life Festival
 	[5811] = 1,	-- Snow Bear Plunge
 	[5835] = 1,	-- The Trial of Five-Clawed Guile
 	[5837] = 1,	-- Lava Foot Stomp
@@ -169,6 +362,7 @@ local EVENTQUESTIDS = {
 EVENTEXPBUFFS = {
 	[91369]	= 1167, -- Jester's Experience Boost Pie
 	[91449]	= 1168, -- Breda's Magnificent Mead
+	[96118]	= 479,	-- Withcmother's Boon -> Witchmother's Whistle
 	[152514]	= 9012,	-- 2021 Jubilee cake
 	[167846]	= 10287,	-- 2022 Jubilee cake
 }
@@ -266,15 +460,20 @@ function Eventor.Eventor_TEST(inpuuut)
 end
 
 -- Refreshes the characters exp buff
+local collectibleCooldownEnds = 0
 local function GiveThatSweetExpBoost( abilityId )
 
 	if	eventorSettings.keepEventBuffsOn  and
-	(eventorSettings.whenOurEventBuffRunsOut or 0) + (eventorSettings.eventBuffsThreshold * 60)	<	os.time()	then
-		if abilityId ~= nil then
-			UseCollectible( EVENTEXPBUFFS[abilityId] )
-		else
-			UseCollectible( EVENTEXPBUFFS[eventorSettings.lastEventBuffId] )
-		end
+	(eventorSettings.whenOurEventBuffRunsOut or 0) + (eventorSettings.eventBuffsThreshold * 60)	<	os.time()	and
+	collectibleCooldownEnds < os.time()	then
+
+			if abilityId and IsCollectibleUsable(EVENTEXPBUFFS[abilityId])	then
+				UseCollectible( EVENTEXPBUFFS[abilityId] )
+				local cooldownRemaining, _ = GetCollectibleCooldownAndDuration(EVENTEXPBUFFS[abilityId])
+				collectibleCooldownEnds = os.time() + cooldownRemaining
+			else
+				UseCollectible( EVENTEXPBUFFS[eventorSettings.lastEventBuffId] )
+			end
 	end
 	ticketAlert()
 end
@@ -290,36 +489,37 @@ end
 activePlayerBuffs = {}
 --	100034	EVENT_EFFECT_CHANGED (integer eventCode, integer changeType, integer effectSlot, string effectName, string unitTag, number beginTime, number endTime, integer stackCount, string iconName, string buffType, integer effectType, integer abilityType, integer statusEffectType, string unitName, integer unitId, integer abilityId, integer sourceUnitType)
 function Eventor.ListenToEventBuffs(eventCode, changeType, effectSlot, effectName, unitTag, beginTime, endTime, stackCount, iconName, buffType, effectType, abilityType, statusEffectType, unitName, unitId, abilityId, sourceUnitType)
-	if not EVENTEXPBUFFS[abilityId] then return end	-- Not a event buff so -> end
+	if not EVENTEXPBUFFS[abilityId] then return end	-- Not an event buff so ending
 
 --	ticketAlert()
 
-	if (beginTime + 7190 < endTime) then	-- 2h buff is 7200 seconds.
+	if (beginTime + 7190 < endTime) then	-- 2h buff is 7200 seconds and logout takes 10 seconds.
 		eventorSettings.lastTimeSomeoneGainedEventBuff = os.time()
 		eventorSettings.lastEventBuffId = abilityId
---		d ( ADDON .. ": EVENT IS ON ")
+		d ( ADDON .. ": " .. zo_iconTextFormat(GetAbilityIcon(abilityId) , "100%", "100%", GetAbilityName(abilityId) ) .. " was gained by " .. unitName)
 	end
 
-	if GetRawUnitName("player") == unitName	then
-		if changeType == EFFECT_RESULT_GAINED then
+	if unitTag == "player"	then
+		if changeType == EFFECT_RESULT_GAINED then	-- Player got an event buff
 			eventorSettings.whenOurEventBuffRunsOut	= os.time() + (endTime - beginTime)
 			activePlayerBuffs[abilityId] = eventorSettings.whenOurEventBuffRunsOut
-		--	d( ADDON .. ": player gained " .. tostring(abilityId) .. "/" .. effectName .. " timeleft:" .. ZO_FormatTimeLargestTwo((endTime - beginTime), TIME_FORMAT_STYLE_DESCRIPTIVE_MINIMAL) )
-		elseif changeType == EFFECT_RESULT_UPDATED then
+			d( ADDON .. ": " .. unitName .. " gained " .. zo_iconTextFormat(GetAbilityIcon(abilityId) , "100%", "100%", GetAbilityName(abilityId) ) .. " timeleft:" .. ZO_FormatTimeLargestTwo((endTime - beginTime), TIME_FORMAT_STYLE_DESCRIPTIVE_MINIMAL) )
+--
+		elseif changeType == EFFECT_RESULT_UPDATED then	-- Player event buff for refreshed
 			eventorSettings.whenOurEventBuffRunsOut	= os.time() + (endTime - beginTime)
 			activePlayerBuffs[abilityId] = eventorSettings.whenOurEventBuffRunsOut
 		--	d( ADDON .. ": players " .. tostring(abilityId) .. "/" .. effectName .. " was refreshed for " .. ZO_FormatTimeLargestTwo((endTime-beginTime), TIME_FORMAT_STYLE_DESCRIPTIVE_MINIMAL))
-		elseif changeType == EFFECT_RESULT_FADED	then
+		elseif changeType == EFFECT_RESULT_FADED	then	-- Player lost event buff
 			activePlayerBuffs[abilityId] = false
 			eventorSettings.whenOurEventBuffRunsOut	= os.time()
 		--	d( ADDON .. ": players " .. tostring(abilityId) .. "/" .. effectName .. " faded" )
 			GiveThatSweetExpBoost(abilityId)
 		end
-	else	-- Someone else is running around with a event buff
+	else	-- Someone else gained a event buff
 		if (changeType == EFFECT_RESULT_GAINED or changeType == EFFECT_RESULT_UPDATED)
-		and	beginTime + 7190 < endTime	-- Someone actually gained the buff
-		and	not activePlayerBuffs[abilityId] then
-		--	d( ADDON .. ": " .. ZO_LinkHandler_CreateLinkWithoutBrackets(unitName, nil, CHARACTER_LINK_TYPE, unitName) .. " ".. tostring(abilityId) .. "/" .. effectName .. " gained(1) or updated(3) =" .. changeType .. " timeleft: " .. ZO_FormatTimeLargestTwo((endTime-beginTime), TIME_FORMAT_STYLE_DESCRIPTIVE_MINIMAL) )
+		and	beginTime + 7190 < endTime	-- 2h buff is 7200 seconds and logout takes 10 seconds.
+		and	not activePlayerBuffs[abilityId] then	-- Player is missing the buff and someone else actually got a new buff.
+--			d( ADDON .. ": " .. ZO_LinkHandler_CreateLinkWithoutBrackets(unitName, nil, CHARACTER_LINK_TYPE, unitName) ..  tostring(abilityId) .. "/" .. effectName .. " gained(1) or updated(3) =" .. changeType .. " timeleft: " .. ZO_FormatTimeLargestTwo((endTime-beginTime), TIME_FORMAT_STYLE_DESCRIPTIVE_MINIMAL) )
 		GiveThatSweetExpBoost(abilityId)
 		end
 	end
@@ -348,7 +548,6 @@ function Eventor.EVENT_ADD_ON_LOADED(_, loadedAddOnName)
 		Eventor.Initialize()
   end
 end
-
 
 
 -- LAM stuff
